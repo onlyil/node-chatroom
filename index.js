db634cd250e9839c82c88230d4ef8d3092f5aebc
Only <lifedamara@163.com>
2018-11-06 10:15:40 +0800

    :construction: Create Chat class

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const Koa = require('koa')
-const router = require('koa-router')
 const logger = require('koa-logger')
 const serve = require('koa-static')
 
 const app = Koa()
 
-
-
 app.use(serve(__dirname + '/public'))
+app.use(logger())
 
 const server = require('http').createServer(app.callback())
 const io = require('socket.io')(server)
