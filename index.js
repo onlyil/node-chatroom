93fa1ac1fdf95d3d8c82fa62858ea57a54d37d4c
Only <lifedamara@163.com>
2018-11-06 20:54:41 +0800

    :construction: Improve index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
 const serve = require('koa-static')
+const Chat = require('./lib/chat')
 
-const app = Koa()
+const app = new Koa()
 
 app.use(serve(__dirname + '/public'))
 app.use(logger())
 
 const server = require('http').createServer(app.callback())
-const io = require('socket.io')(server)
 
-io.on('connection', () => {
-  // 
-})
+const chat = new Chat()
+chat.initialSocket(server)
 
 server.listen(5200)
 console.log('listening on port 5200')
